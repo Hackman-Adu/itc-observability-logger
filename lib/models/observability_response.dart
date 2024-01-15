721643367a98d747a0ad5f166b3fc090ce89c3be
Hackman Adu Gyamfi <adugyamfihackman31@gmail.com>
2024-01-15 09:41:57 +0000

    removed dummy code

--- lib/models/observability_response.dart
+++ lib/models/observability_response.dart
@@ -5,10 +5,6 @@ class ObservabilityResponse {
   Object? responseBody;
   ObservabilityResponse({this.responseBody, this.status});
 
-  static ObservabilityResponse fromApi(Map<String, dynamic> json) {
-    return ObservabilityResponse();
-  }
-
   String get printIcon =>
       status == ResponseStatus.succes ? "✅✅✅✅✅✅✅✅✅✅" : "❌❌❌❌❌❌❌❌❌❌";
 
